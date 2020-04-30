275cc36ce09fdfbb2287f014bff9d8aeb962f413
hyodofumiya <f.hyodo.0123@gmail.com>
2020-04-30 20:06:55 +0900

    wake

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     const useNodeJS = true;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1654154094-L2PYjd9P";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
@@ -17,7 +17,6 @@ window.onload = function() {
                 initializeLiffOrDie(myLiffId);
             })
             .catch(function(error) {
-                document.getElementById("liffAppContent").classList.add('hidden');
                 document.getElementById("nodeLiffIdErrorMessage").classList.remove('hidden');
             });
     } else {
